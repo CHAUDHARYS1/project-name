d544763176166720d4e88a877a381cddc0e8e3c1
Jaya <jaya.vedantam@gmail.com>
2021-11-21 16:38:09 -0800

    adding queries and mutattion on server side

--- server/models/Job.js
+++ server/models/Job.js
@@ -1,64 +1,68 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const { Schema, model } = mongoose;
-const dateFormat = require('../utils/dateFormat');
+const dateFormat = require("../utils/dateFormat");
 
 const jobSchema = new Schema(
   {
     jobtitle: {
       type: String,
-      required: 'You need to leave a title!',
+      required: "You need to leave a title!",
       minlength: 1,
-      maxlength: 280
+      maxlength: 280,
     },
     createdAt: {
       type: Date,
       default: Date.now,
-      get: timestamp => dateFormat(timestamp)
+      get: (timestamp) => dateFormat(timestamp),
     },
     username: {
       type: String,
-      required: true
+      required: false,
     },
     description: {
       type: String,
-      required: true
+      required: true,
     },
     company: {
       type: String,
-      required: true
+      required: false,
     },
     requirements: {
       type: String,
-      required: true
+      required: true,
     },
     linktowebsite: {
       type: String,
-      required: false
+      required: false,
     },
     roletype: {
       type: String,
-      required: true
+      required: true,
     },
     benefits: {
       type: String,
-      required: false
+      required: false,
     },
     salary: {
       type: String,
-      required: true
+      required: true,
     },
     additionalinfo: {
       type: String,
-      required: false
+      required: false,
+    },
+    resume: {
+      type: Buffer,
+      required: false,
     },
   },
   {
     toJSON: {
-      getters: true
-    }
+      getters: true,
+    },
   }
 );
 
-const Job = mongoose.model('Job', jobSchema);
+const Job = mongoose.model("Job", jobSchema);
 
 module.exports = Job;
