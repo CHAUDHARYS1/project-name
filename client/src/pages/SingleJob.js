d03a348ef8e6a0085bbf7d855cea97c7c2b11d70
Jaya <jaya.vedantam@gmail.com>
2021-11-26 13:47:55 -0800

    single job, populate fields

--- client/src/pages/SingleJob.js
+++ client/src/pages/SingleJob.js
@@ -17,9 +17,6 @@ const SingleJob = props => {
   
     const job = data?.job || {};
 
-    console.log('data', data)
-    console.log('job', job)
-
     return (
         <div>
             <Container>
@@ -29,11 +26,11 @@ const SingleJob = props => {
                         <Icon name='building' />
                         <Header.Content>
                             Job Title: {job.jobtitle}
-                            <Header.Subheader>Company Name:</Header.Subheader>
-                            <Header.Subheader>Website:</Header.Subheader>
-                            <Header.Subheader>Date Posted:</Header.Subheader>
+                            <Header.Subheader>Company Name: {job.company}</Header.Subheader>
+                            <Header.Subheader>Website:{job.linktowebsite}</Header.Subheader>
+                            <Header.Subheader>Date Posted: </Header.Subheader>
                             <Header.Subheader>Start Date:</Header.Subheader>
-                            <Header.Subheader>Type of Position:</Header.Subheader>
+                            <Header.Subheader>Type of Position:{job.roletype}</Header.Subheader>
                         </Header.Content>
                     </Header>
                     <div>
@@ -41,58 +38,26 @@ const SingleJob = props => {
                             Company Description:
                         </h3>
                         <p>
-                            Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
-                            Maecenas porttitor congue massa. Fusce posuere, magna sed pulvinar
-                            ultricies, purus lectus malesuada libero, sit amet commodo magna
-                            eros quis urna. Nunc viverra imperdiet enim. Fusce est.
-                            Vivamus a tellus. Pellentesque habitant morbi tristique senectus et
-                            netus et malesuada fames ac turpis egestas. Proin pharetra nonummy pede. Mauris et orci.
-                            Aenean nec lorem. In porttitor. Donec laoreet nonummy augue.
-                            Suspendisse dui purus, scelerisque at, vulputate vitae, pretium mattis, nunc.
-                            Mauris eget neque at sem venenatis eleifend. Ut nonummy.
+                          {job.description}
                         </p>
                         <h3>
                             Job Description:
                         </h3>
                         <p>
-                            Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
-                            Maecenas porttitor congue massa. Fusce posuere, magna sed pulvinar
-                            ultricies, purus lectus malesuada libero, sit amet commodo magna
-                            eros quis urna. Nunc viverra imperdiet enim. Fusce est.
-                            Vivamus a tellus. Pellentesque habitant morbi tristique senectus et
-                            netus et malesuada fames ac turpis egestas. Proin pharetra nonummy pede. Mauris et orci.
-                            Aenean nec lorem. In porttitor. Donec laoreet nonummy augue.
-                            Suspendisse dui purus, scelerisque at, vulputate vitae, pretium mattis, nunc.
-                            Mauris eget neque at sem venenatis eleifend. Ut nonummy.
+                        {job.additionalinfo}
                         </p>
                         <h3>
                             Requirements:
                         </h3>
                         <p>
-                            Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
-                            Maecenas porttitor congue massa. Fusce posuere, magna sed pulvinar
-                            ultricies, purus lectus malesuada libero, sit amet commodo magna
-                            eros quis urna. Nunc viverra imperdiet enim. Fusce est.
-                            Vivamus a tellus. Pellentesque habitant morbi tristique senectus et
-                            netus et malesuada fames ac turpis egestas. Proin pharetra nonummy pede. Mauris et orci.
-                            Aenean nec lorem. In porttitor. Donec laoreet nonummy augue.
-                            Suspendisse dui purus, scelerisque at, vulputate vitae, pretium mattis, nunc.
-                            Mauris eget neque at sem venenatis eleifend. Ut nonummy.
+                  {job.requirements}
                         </p>
                         <h3>
                             Salary/Pay and Benefits:
                         </h3>
-                        <p>Starting Salary/Pay: </p>
+                        <p>Starting Salary/Pay: {job.salary} </p>
                         <p>
-                            Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
-                            Maecenas porttitor congue massa. Fusce posuere, magna sed pulvinar
-                            ultricies, purus lectus malesuada libero, sit amet commodo magna
-                            eros quis urna. Nunc viverra imperdiet enim. Fusce est.
-                            Vivamus a tellus. Pellentesque habitant morbi tristique senectus et
-                            netus et malesuada fames ac turpis egestas. Proin pharetra nonummy pede. Mauris et orci.
-                            Aenean nec lorem. In porttitor. Donec laoreet nonummy augue.
-                            Suspendisse dui purus, scelerisque at, vulputate vitae, pretium mattis, nunc.
-                            Mauris eget neque at sem venenatis eleifend. Ut nonummy.
+                      {job.benefits}
                         </p>
                     </div>
                 </Segment>
